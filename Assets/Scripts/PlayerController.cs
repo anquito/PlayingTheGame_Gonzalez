6762dea8fc06dba0ace001c8a1078318114634c4
anquito <r-andg@hotmail.com>
2020-04-20 11:02:11 -0700

    Lesson 4A

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float forSpeed;
+    private GameObject focalPoint;
+    private Renderer playerRend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRend = GetComponent<Renderer>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * verticalInput * forSpeed);
+        if(verticalInput > 0)
+        {
+            playerRend.material.color = new Color(1 - verticalInput, 1, 1 - verticalInput);
+        }
+        else
+        {
+            playerRend.material.color = new Color(1 + verticalInput, 1, 1 + verticalInput);
+        }
+    }
+}
