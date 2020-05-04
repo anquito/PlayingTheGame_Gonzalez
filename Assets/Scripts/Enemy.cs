4c8005a7e8cb817b65a435a42822455bb9e55bf5
anquito <r-andg@hotmail.com>
2020-05-04 11:16:43 -0700

    Lesson 4D

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour
     private Rigidbody enemyRb;
     private GameObject player;
     public float speed;
+    private int outOfBounds = -10;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +18,12 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 moveDir = player.transform.position - transform.position.normalized;
+        Vector3 moveDir = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(moveDir * speed);
+
+        if (transform.position.y < outOfBounds)
+        {
+            Destroy(gameObject);
+        }
     }
 }
